5f1f38cb3d31f2e6febe67204a70cf885ee63ecc
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-02-27 22:24:17 +0530

    feat setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className='bg-red-500'> 
+     <Header></Header>
+    </div>
+  );
+}
+
+export default App;
+
+
+// Header
+//   -logo 
+//   - Nav items 
+// Body 
+//    - search
+//    - restro <Container></Container>
+// // footer 
+//       -copyright
+//       - Link
+//       - Adreess
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='heaer'>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+       
+    </div>
+  );
+}
+export default Header;
