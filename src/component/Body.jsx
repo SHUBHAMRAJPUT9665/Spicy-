99d8b214ce3dd546e54974d4279b43a36ba9aa00
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-03-02 20:22:55 +0530

    render effect

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -3,28 +3,34 @@ import RestaruntCard from "./RestaruntCard";
 import Header from "./Header";
 import resList from "../utils/data";
 import { useState } from "react";
+import { useEffect } from "react";
 
 function Body() {
   const [data, setdata] = useState(null);
-  console.log(<Body />)
+  const fetchData = async () => {
+    const data = fetch("www.goole.com");
+    const jsonData = await data.json();
+    
+  }
+  useEffect(() => {
+   console. log("useeffect called...");
+  }, []);
   return (
     <div>
       <div>
         <button
-          onClick={() => {
+            onClick={() => {
             const data2 = resList.filter((res) => res.data.avgRating > 4);
             setdata(data2);
-            console.log(data);
           }}
           className=" rounded-lg m-3 mb-0 px-6 text-center p-4 bg-amber-400"
-        >
-          {data?"Filtred Restaurant":"Filter"}
+        > {data?"Filtred Restaurant":"Filter"}
         </button>
       </div>
       <div className="flex justify-around mt-10 flex-wrap ">
         {data
           ? data.map((restaurant) => (
-              <RestaruntCard key={restaurant.data.id} resData={restaurant} />
+              <RestaruntCard key={restaurant.data.id}  resData={restaurant} />
             ))
           : resList.map((restaurant) => (
               <RestaruntCard key={restaurant.data.id} resData={restaurant} />
